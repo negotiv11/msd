94d30c7bc972025ee252accac47817d0227a1712
negotiv11 <denis2103denis2103@gmail.com>
2025-06-21 22:34:38 +0300

    Update key.js

--- key.js
+++ key.js
@@ -1,1 +1,241 @@
-
+{
+        "token": "NEREST_1ez_mod_NerestProject_mod_ez1023131",
+        "active": true,
+        "user": "xd",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2025-12-31"
+    },
+    {
+        "token": "1vs1ss1",
+        "active": true,
+        "user": "I",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2029-01-01"
+    },
+    {
+        "token": "NEREST_1ez_mod_NerestProject_mod_ez11",
+        "active": true,
+        "user": "play",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2025-05-09"
+    },
+    {
+        "token": "gf4hi7899",
+        "active": true,
+        "user": "Никсик",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2025-03-15"
+    },
+    {
+        "token": "jkl0122",
+        "active": true,
+        "user": "user5",
+        "devices": 1,
+        "max_devices": 3,
+        "expiry_date": "2025-11-11"
+    },
+    {
+        "token": "mno3455",
+        "active": true,
+        "user": "user6",
+        "devices": 1,
+        "max_devices": 4,
+        "expiry_date": "2025-07-30"
+    },
+    {
+        "token": "pqr6781",
+        "active": false,
+        "user": "user7",
+        "devices": 2,
+        "max_devices": 2,
+        "expiry_date": "2024-06-01"
+    },
+    {
+        "token": "stu901",
+        "active": true,
+        "user": "user8",
+        "devices": 3,
+        "max_devices": 3,
+        "expiry_date": "2025-09-01"
+    },
+    {
+        "token": "vwx2341",
+        "active": true,
+        "user": "user91",
+        "devices": 1,
+        "max_devices": 5,
+        "expiry_date": "2025-08-20"
+    },
+    {
+        "token": "yza567",
+        "active": true,
+        "user": "user10",
+        "devices": 0,
+        "max_devices": 2,
+        "expiry_date": "2025-04-17"
+    },
+    {
+        "token": "bcd890",
+        "active": false,
+        "user": "user11",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2023-12-25"
+    },
+    {
+        "token": "efg123",
+        "active": true,
+        "user": "user12",
+        "devices": 1,
+        "max_devices": 4,
+        "expiry_date": "2025-02-28"
+    },
+    {
+        "token": "hij456",
+        "active": true,
+        "user": "user13",
+        "devices": 3,
+        "max_devices": 5,
+        "expiry_date": "2025-06-30"
+    },
+    {
+        "token": "klm789",
+        "active": true,
+        "user": "user14",
+        "devices": 0,
+        "max_devices": 3,
+        "expiry_date": "2025-01-01"
+    },
+    {
+        "token": "nop012",
+        "active": false,
+        "user": "user15",
+        "devices": 2,
+        "max_devices": 2,
+        "expiry_date": "2024-02-15"
+    },
+    {
+        "token": "qrs345",
+        "active": true,
+        "user": "user16",
+        "devices": 0,
+        "max_devices": 5,
+        "expiry_date": "2025-10-10"
+    },
+    {
+        "token": "tuv678",
+        "active": true,
+        "user": "user17",
+        "devices": 1,
+        "max_devices": 2,
+        "expiry_date": "2025-03-20"
+    },
+    {
+        "token": "wxy901",
+        "active": false,
+        "user": "user18",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2024-07-25"
+    },
+    {
+        "token": "zab234",
+        "active": true,
+        "user": "user19",
+        "devices": 3,
+        "max_devices": 3,
+        "expiry_date": "2025-05-10"
+    },
+    {
+        "token": "cde567",
+        "active": true,
+        "user": "user20",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2025-06-01"
+    },
+    {
+        "token": "fgh890",
+        "active": true,
+        "user": "user21",
+        "devices": 0,
+        "max_devices": 2,
+        "expiry_date": "2025-11-05"
+    },
+    {
+        "token": "ijk123",
+        "active": true,
+        "user": "user22",
+        "devices": 0,
+        "max_devices": 4,
+        "expiry_date": "2025-01-15"
+    },
+    {
+        "token": "lmn456",
+        "active": false,
+        "user": "user23",
+        "devices": 1,
+        "max_devices": 1,
+        "expiry_date": "2023-11-30"
+    },
+    {
+        "token": "opq789",
+        "active": true,
+        "user": "user24",
+        "devices": 2,
+        "max_devices": 3,
+        "expiry_date": "2025-08-25"
+    },
+    {
+        "token": "rst012",
+        "active": true,
+        "user": "user25",
+        "devices": 0,
+        "max_devices": 2,
+        "expiry_date": "2025-02-14"
+    },
+    {
+        "token": "uvw345",
+        "active": false,
+        "user": "user26",
+        "devices": 1,
+        "max_devices": 2,
+        "expiry_date": "2024-09-09"
+    },
+    {
+        "token": "xyz678",
+        "active": true,
+        "user": "user27",
+        "devices": 0,
+        "max_devices": 3,
+        "expiry_date": "2025-05-25"
+    },
+    {
+        "token": "abc901",
+        "active": true,
+        "user": "user28",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2025-03-10"
+    },
+    {
+        "token": "def234",
+        "active": true,
+        "user": "user29",
+        "devices": 1,
+        "max_devices": 5,
+        "expiry_date": "2025-12-01"
+    },
+    {
+        "token": "ghi678",
+        "active": false,
+        "user": "user30",
+        "devices": 0,
+        "max_devices": 1,
+        "expiry_date": "2023-10-01"
+    }
+]
